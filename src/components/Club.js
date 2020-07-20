4c37597d375859798b08a2bb78962ac225651d7b
Srishti Nath <srishti.nath@gmail.com>
2020-07-20 12:38:35 -0400

    done finally

--- src/components/Club.js
+++ src/components/Club.js
@@ -11,11 +11,11 @@ class Club extends Component {
         return ( 
             <div className="club-card" onClick={this.handleClick}>
                 {this.props.club.name}
-                {this.props.club ? 
+                {/* {this.props.club ? 
                 <p>Number of members: {this.props.club.users_count} </p> 
                 :
                 <p>Number of members: 0 </p>
-                }
+                } */}
             </div>
          );
     }
